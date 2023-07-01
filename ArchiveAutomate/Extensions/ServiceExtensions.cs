2a53ae096584d469198cc81fb65c4f74cbc79ccf
rrfarmer <ryanfarmer@mac.com>
2023-07-01 14:37:57 -0400

    Initial Configs

---
+++ ArchiveAutomate/Extensions/ServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace ArchiveAutomate.Extensions
+{
+    public static class ServiceExtensions
+    {
+
+        public static void ConfigureCors(this IServiceCollection services) => 
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                        builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .WithExposedHeaders("X-Pagination"));
+            });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+
+    }
+}
